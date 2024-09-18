d2d28ee68bb3744d29d2432afb612e780b76c109
Karthik Ramadass <ramadak@hickory.accre.vanderbilt.edu>
2024-09-18 13:26:18 -0500

    launch script in bash

---
+++ launch_rcp.sh
@@ -0,0 +1,3 @@
+source /home/ramadak/environments/scraper/bin/activate
+
+python /home/ramadak/git_repo/scraper/real_clear_politics_scrap.py >> /home/ramadak/git_repo/scraper/Elections_scrape_rcp.txt
